e2737340f8c0502b1478389a9a6d9a8d80ebc541
petermphundi080-ui <petermphundi080@gmail.com>
2026-08-26 14:45:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const nodemailer = require("nodemailer");
 const cookie = require("cookie");
 const crypto = require("crypto");
 const bcrypt = require("bcryptjs");
-const usersStore = require("./lib/usersStore");
-const contentStore = require("./lib/contentStore");
-const pageContentStore = require("./lib/pageContentStore");
+const usersStore = require("./lib/usersstore");
+const contentStore = require("./lib/contentstore");
+const pageContentStore = require("./lib/pagecontentstore");
 
 require("dotenv").config();
 
